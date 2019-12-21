77aebaeb8a122f76ef400ecf843bebe9c09bccdd
Koen Zandberg <koen@bergzand.net>
2019-12-21 14:26:45 +0100

    modules/hal: Add PineTime high level HAL functions

---
+++ modules/hal/hal.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018 Koen Zandberg <koen@bergzand.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include "hal.h"
+#include "log.h"
+
+#include "ili9341.h"
+#include "ili9341_params.h"
+
+#include "lvgl.h"
+
+/* ili9341 device driver state */
+static ili9341_t _disp_dev;
+
+void *hal_display_get_context(void)
+{
+    return (display_t*)&_disp_dev;
+}
+
+/* Should be called somewhere during auto_init */
+void hal_init(void)
+{
+    if (ili9341_init(&_disp_dev, &ili9341_params[0]) == 0) {
+        puts("[OK]");
+    }
+    else {
+        LOG_ERROR("[ILI9341]: Device initialization failed");
+    }
+}
---
+++ modules/hal/hal.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018 Koen Zandberg <koen@bergzand.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef APP_HAL
+#define APP_HAL
+
+#include <stdint.h>
+#include "ili9341.h"
+#include "event.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void display_t;
+
+static inline void hal_display_flush(display_t *display, uint16_t x1,
+                                     uint16_t x2, uint16_t y1, uint16_t y2,
+                                     uint16_t *color)
+{
+    ili9341_t *disp = (ili9341_t*)display;
+    ili9341_pixmap(disp, x1, x2, y1, y2, color);
+}
+
+display_t *hal_display_get_context(void);
+
+void hal_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_HAL */
