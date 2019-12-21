af481dd9b747e4fcf5c6c9332f0998e07a007d2b
Koen Zandberg <koen@bergzand.net>
2019-12-21 21:24:11 +0100

    PineTime: Update main function code

--- apps/pinetime/main.c
+++ apps/pinetime/main.c
@@ -10,8 +10,6 @@
 #include "xtimer.h"
 #include "lvgl.h"
 
-#include "xtimer.h"
-
 #include "shell.h"
 #include "msg.h"
 
@@ -19,7 +17,7 @@
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 extern lv_disp_t *hal_init(void);
-extern int lvgl_thread_create(lv_disp_t *display);
+int lvgl_thread_create(void);
 
 int main(void)
 {
@@ -27,20 +25,13 @@ int main(void)
     LOG_DEBUG("Starting PineTime application");
     lv_init();
 
-    lv_disp_t * disp = hal_init();
-    if (disp == NULL) {
-        return 0;
-    }
-    lvgl_thread_create(disp);
+    hal_init();
+    lvgl_thread_create();
 
     /* start shell */
     puts("All up, running the shell now");
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
-
-    while (1) {
-        xtimer_sleep(10);
-    }
 
     return 0;
 }
