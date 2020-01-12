84f660a4325b1a6340f92bf5e18b74c0b94307ab
Koen Zandberg <koen@bergzand.net>
2020-01-12 16:10:57 +0100

    fonts: Initial include of font module

---
+++ modules/fonts/include/fonts/noto_sans_numeric_60.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2018 Koen Zandberg <koen@bergzand.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef _APP_FONTS_NOTO_SANS_NUMERIC_60_H
+#define _APP_FONTS_NOTO_SANS_NUMERIC_60_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern lv_font_t noto_sans_numeric_60;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
