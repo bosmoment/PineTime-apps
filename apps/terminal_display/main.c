8cda314d51a83b7a7bc7c012e38b7cc09148e1ef
Koen Zandberg <koen@bergzand.net>
2020-01-18 23:44:33 +0100

    apps/terminal: Add application using home_terminal

---
+++ apps/terminal_display/main.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2018 Koen Zandberg <koen@bergzand.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include "log.h"
+#include "xtimer.h"
+#include "lvgl.h"
+#include "controller.h"
+#include "bleman.h"
+
+#include "shell.h"
+#include "msg.h"
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+extern lv_disp_t *hal_init(void);
+int lvgl_thread_create(void);
+
+int main(void)
+{
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+    LOG_DEBUG("Starting PineTime application");
+    lv_init();
+
+    hal_init();
+    lvgl_thread_create();
+    bleman_thread_create();
+
+    controller_thread_create();
+    /* start shell */
+    puts("All up, running the shell now");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
---
+++ apps/terminal_display/modules.mk
@@ -0,0 +1,11 @@
+USEMODULE += gui
+USEMODULE += controller
+USEMODULE += widget
+USEMODULE += bleman
+USEMODULE += hal
+USEMODULE += util
+
+USEMODULE += shell
+USEMODULE += shell_commands
+USEMODULE += ps
+USEMODULE += schedstatistics
