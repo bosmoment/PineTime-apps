7c4903b094424f2de8024115b50c8c1ce6dc65e2
Koen Zandberg <koen@bergzand.net>
2020-02-29 14:25:47 +0100

    menu: fix widget loop iteration

--- widgets/menu_tiles/screen_menu.c
+++ widgets/menu_tiles/screen_menu.c
@@ -67,7 +67,7 @@ lv_obj_t *screen_menu_create(void)
     lv_obj_align(list1, NULL, LV_ALIGN_CENTER, 0, 0);
 
     lv_obj_t *list_btn;
-    for (size_t i=2; i < ARRAY_SIZE(widgets_installed); i++) {
+    for (size_t i=0; i < ARRAY_SIZE(widgets_installed); i++) {
         widget_t *widget = widgets_installed[i];
         LOG_INFO("[menu_tiles]: adding button for %s\n", widget->spec->name);
         list_btn = lv_list_add_btn(list1, NULL, widget_get_label(widget));
